e12a68b4b7d90f1513b68b5cf97ccc20ebd93334
inzhgeos <inzhgeos@gmail.com>
2019-03-18 15:12:12 +0200

    Update Esp32-lwmqtt.ino
    Перевод комментариев на русский язык

--- examples/Esp32-lwmqtt/Esp32-lwmqtt.ino
+++ examples/Esp32-lwmqtt/Esp32-lwmqtt.ino
@@ -15,7 +15,7 @@
 #include "esp32-mqtt.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  // код размещенный здесь будет выполнен единожды при запуске:
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   setupCloudIoT();
@@ -24,13 +24,13 @@ void setup() {
 unsigned long lastMillis = 0;
 void loop() {
   mqttClient->loop();
-  delay(10);  // <- fixes some issues with WiFi stability
+  delay(10);  // <- исправляет некоторые проблемы со стабильностью WiFi соединения
 
   if (!mqttClient->connected()) {
     connect();
   }
 
-  // publish a message roughly every second.
+  // публикует сообщение примерно каждую секунду.
   if (millis() - lastMillis > 1000) {
     lastMillis = millis();
     publishTelemetry(getDefaultSensor());
